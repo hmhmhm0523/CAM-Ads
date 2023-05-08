22c958d713c1a40d1db5648d5977f56571c9db58
hmhmhm0523 <mhuang6@ncsu.edu>
2023-05-08 19:32:45 -0400

    Added some animation

--- info-panel-on-login-screen/src/App.js
+++ info-panel-on-login-screen/src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import logo from './images/Logo.svg';
 import './App.css';
+import video1 from './images/Protect.mp4';
+import video2 from './images/Safeguard.mp4';
+import video3 from './images/SaveMoney.mp4';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h2>
+          Simplify elevated access requests with least-privilege controls
+        </h2>
+        <p>Eliminate shared admin passwords and protect customers from security threats</p>
+        <div className="Container">
+          <div className="Item">
+            <video loop muted src={video1} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()}></video>
+            <h4>Protect Privileged Credentials</h4>
+          </div>
+          <div className="Item">
+            <video loop muted src={video2} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()}></video>
+            <h4>Safeguard Your Environment</h4>
+            </div>
+          <div className="Item">
+            <video loop muted src={video3} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()}></video>
+            <h4>Save Time and Money</h4></div>
+        </div>
+
+        <button
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Find Out More
+        </button>
       </header>
     </div>
   );
